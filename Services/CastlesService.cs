b40ed798d380795fa70fcb1fe9e6b73b03d796bd
john87barker <john87barker@gmail.com>
2021-09-08 09:48:33 -0600

    Init

---
+++ Services/CastlesService.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using knight.Models;
+
+namespace knight.Services
+{
+  public class CastlesService
+  {
+    internal Castle Get(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal IEnumerable<Castle> Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Castle Create(Castle newCastle)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Castle Edit(Castle updatedCastle)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
